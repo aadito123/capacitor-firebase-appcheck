77d3d14f894a3677e831aa211349d684ec31f02b
aadito123 <aaditolkar123@gmail.com>
2022-09-27 12:35:22 +0800

    ios test

--- ios/Plugin/AppCheck.swift
+++ ios/Plugin/AppCheck.swift
@@ -1,8 +1,49 @@
+import Capacitor
+import FirebaseCore
+import FirebaseAppCheck
 import Foundation
 
-@objc public class AppCheck: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+/// Please read the Capacitor iOS Plugin Development Guide
+/// here: https://capacitorjs.com/docs/plugins/ios
+@objc(AppCheckPlugin)
+public class AppCheckPlugin: CAPPlugin {
+  private let appAttestProvider = AppAttestProvider(app: FirebaseApp.app())
+  private let debugProvider = AppCheckDebugProvider(app: FirebaseApp.app())
+  private let debugMode = false
+
+  @objc func load() {
+
+  }
+
+  @objc func getToken(_ call: CAPPluginCall) {
+    if debugMode {
+      debugProvider.getToken { token, error in
+        if let error = error {
+          call.reject(error.localizedDescription)
+        } else if let token = token {
+          call.resolve([
+            "token": token.token,
+            "expireTimeMillis": String(Int64(token.expirationDate.timeIntervalSince1970 * 1000.0)),
+          ])
+        }
+      }
+    } else {
+      appAttestProvider.getToken { token, error in
+        if let error = error {
+          call.reject(error.localizedDescription)
+        } else if let token = token {
+          call.resolve([
+            "token": token.token,
+            "expireTimeMillis": String(Int64(token.expirationDate.timeIntervalSince1970 * 1000.0)),
+          ])
+        }
+      }
     }
+  }
+
+    @objc func enableDebug(_ call: CAPPluginCall) {
+        debugMode = true
+        call.resolve()
+    }
+
 }
