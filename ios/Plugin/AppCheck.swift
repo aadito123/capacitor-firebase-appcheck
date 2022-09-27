12040f7441590775fabd036ff29836cad6932622
aadito123 <aaditolkar123@gmail.com>
2022-09-27 01:50:23 -0700

    ios issues fix

--- ios/Plugin/AppCheck.swift
+++ ios/Plugin/AppCheck.swift
@@ -1,49 +1,73 @@
 import Capacitor
-import FirebaseCore
 import FirebaseAppCheck
+import FirebaseCore
 import Foundation
 
 /// Please read the Capacitor iOS Plugin Development Guide
 /// here: https://capacitorjs.com/docs/plugins/ios
+@available(iOS 14.0, *)
 @objc(AppCheckPlugin)
 public class AppCheckPlugin: CAPPlugin {
-  private let appAttestProvider = AppAttestProvider(app: FirebaseApp.app())
-  private let debugProvider = AppCheckDebugProvider(app: FirebaseApp.app())
-  private let debugMode = false
+  private var appAttestProvider: AppAttestProvider? = nil
+  private var debugProvider: AppCheckDebugProvider? = nil
+  private var debugMode = false
+  private var errorMessage: String? = nil
 
-  @objc func load() {
-
+  override public func load() {
+    NSLog("FirebaseAppCheck loading...")
+    FirebaseApp.configure()
+    if let firebaseAppInst = FirebaseApp.app() {
+      appAttestProvider = AppAttestProvider(app: firebaseAppInst)
+      debugProvider = AppCheckDebugProvider(app: firebaseAppInst)
+      NSLog("Firebase Debug Token: " + (debugProvider?.localDebugToken())!)
+    } else {
+      errorMessage = "FirebaseApp instance is null"
+    }
+    NSLog("FirebaseAppCheck loaded")
   }
 
   @objc func getToken(_ call: CAPPluginCall) {
-    if debugMode {
+    NSLog("FirebaseAppCheck ")
+    if let errorMessage = errorMessage {
+      call.reject(errorMessage)
+      return
+    }
+    if debugMode, let debugProvider = debugProvider {
       debugProvider.getToken { token, error in
         if let error = error {
+          NSLog("FirebaseAppCheck rejecting (1)")
           call.reject(error.localizedDescription)
         } else if let token = token {
           call.resolve([
             "token": token.token,
             "expireTimeMillis": String(Int64(token.expirationDate.timeIntervalSince1970 * 1000.0)),
+          ])
+        }
+      }
+    } else if let appAttestProvider = appAttestProvider {
+      appAttestProvider.getToken { token, error in
+        if let error = error {
+          NSLog("FirebaseAppCheck rejecting (2)")
+          call.reject(error.localizedDescription)
+        } else if let token = token {
+          call.resolve([
+            "token": token.token,
+            "expireTimeMillis": String(
+              Int64(token.expirationDate.timeIntervalSince1970 * 1000.0)),
           ])
         }
       }
     } else {
-      appAttestProvider.getToken { token, error in
-        if let error = error {
-          call.reject(error.localizedDescription)
-        } else if let token = token {
-          call.resolve([
-            "token": token.token,
-            "expireTimeMillis": String(Int64(token.expirationDate.timeIntervalSince1970 * 1000.0)),
-          ])
-        }
-      }
+      NSLog("FirebaseAppCheck rejecting (3)")
+      call.reject("Providers are null")
     }
   }
 
-    @objc func enableDebug(_ call: CAPPluginCall) {
-        debugMode = true
-        call.resolve()
-    }
+  @objc func enableDebug(_ call: CAPPluginCall) {
+    NSLog("FirebaseAppCheck enableDebug")
+    debugMode = true
+    call.resolve()
+  }
 
 }
+
